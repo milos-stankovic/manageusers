0f3a89997dacd51aeb0b881d257b9546f8ebf0bc
shomsy <milosharkor@hotmail.com>
2018-01-29 18:28:56 +0100

    Credential missmatch bug fixed (Removed default hash string from factory). Validation errors added on User create and update forms.

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -12,23 +12,31 @@
 
         {{ Form::open(array('url' => 'users')) }}
 
-        <div class="form-group">
+        <div class="form-group has-feedback{{ $errors->has('name') ? ' has-error' : '' }}">
             {{ Form::label('name', 'Name') }}
             {{ Form::text('name', '', array('class' => 'form-control')) }}
+            @if ($errors->has('name'))
+                <span class="help-block">{{ $errors->first('name') }}</span>
+            @endif
         </div>
 
-        <div class="form-group">
+        <div class="form-group has-feedback{{ $errors->has('email') ? ' has-error' : '' }}">
             {{ Form::label('email', 'Email') }}
             {{ Form::email('email', '', array('class' => 'form-control')) }}
+            @if ($errors->has('email'))
+                <span class="help-block">{{ $errors->first('email') }}</span>
+            @endif
         </div>
 
-        <div class="form-group">
+        <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
             {{ Form::label('password', 'Password') }}<br>
             {{ Form::password('password', array('class' => 'form-control')) }}
-
+            @if ($errors->has('password'))
+                <span class="help-block">{{ $errors->first('password') }}</span>
+            @endif
         </div>
 
-        <div class="form-group">
+        <div class="form-group has-feedback{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
             {{ Form::label('password', 'Confirm Password') }}<br>
             {{ Form::password('password_confirmation', array('class' => 'form-control')) }}
 
